d9c107a3806926f39d1187ad4cf3ac00db7c8719
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2023-05-21 16:04:04 +0530

    Created Generic Singleton

---
+++ Assets/Scripts/GenericSingleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TankBattle.Singleton
+{
+    public class GenericSingleton<T> : MonoBehaviour where T : GenericSingleton<T>
+    {
+
+        private static T instance;
+        public static T Instance { get { return instance; } }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerTank.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace TankBattle.Singleton
+{
+	public class PlayerTank : GenericSingleton<PlayerTank>
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
